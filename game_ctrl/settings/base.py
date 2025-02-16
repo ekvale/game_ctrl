a18083f96392a33127d06d37cb4d341a5304eaab
Anon A Mouse <lionsmane@protonmail.com>
2025-02-16 11:07:13 -0600

    generate marketing

--- game_ctrl/settings/base.py
+++ game_ctrl/settings/base.py
@@ -46,7 +46,9 @@ ROOT_URLCONF = 'game_ctrl.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- game_ctrl/urls.py
+++ game_ctrl/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', TemplateView.as_view(template_name='registration/register.html'), name='register'),
+    path('test-video/', TemplateView.as_view(template_name='marketing/test_video.html'), name='test_video'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
