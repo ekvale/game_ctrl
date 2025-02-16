8b8e008ad936e101bc6ab375d4fe61a762d5b4d4
Anon A Mouse <lionsmane@protonmail.com>
2025-02-16 11:09:30 -0600

    generate marketing

--- game_ctrl/settings/base.py
+++ game_ctrl/settings/base.py
@@ -46,9 +46,7 @@ ROOT_URLCONF = 'game_ctrl.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
