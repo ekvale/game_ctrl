7f54d94a5d543187c55990603b509144eaba4f72
Anon A Mouse <lionsmane@protonmail.com>
2025-02-16 11:23:31 -0600

    generate marketing

--- game_ctrl/urls.py
+++ game_ctrl/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView, TemplateView
 from .health_checks import health_check
 from django.contrib.auth import views as auth_views
+from django.shortcuts import render
+
+def test_video(request):
+    return render(request, 'marketing/test_video.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,5 +20,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', TemplateView.as_view(template_name='registration/register.html'), name='register'),
-    path('test-video/', TemplateView.as_view(template_name='marketing/test_video.html'), name='test_video'),
+    path('test-video/', test_video, name='test_video'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
