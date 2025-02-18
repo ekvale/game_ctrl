0e3bf2df6618c8125987bb60e8cf8bdf30e2aded
Anon A Mouse <lionsmane@protonmail.com>
2025-02-17 22:17:42 -0600

    Update home.html

--- game_ctrl/settings/base.py
+++ game_ctrl/settings/base.py
@@ -101,8 +101,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Media files
