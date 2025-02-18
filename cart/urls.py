6c3a295c6eed7ef32c3e4d344e1da5567dc999b7
Anon A Mouse <lionsmane@protonmail.com>
2025-02-17 19:24:28 -0600

    ITS WORKING! adding sample data

--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-    path('add/', views.cart_add, name='cart_add'),
+    path('add/', views.add_to_cart, name='add_to_cart'),
     path('remove/<int:controller_id>/', views.cart_remove, name='cart_remove'),
     path('', views.cart_detail, name='cart_detail'),
 ]
