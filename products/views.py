c5c2fc5bcc78a8d3ffdb5bd3fc122d4d68c398ff
Anon A Mouse <lionsmane@protonmail.com>
2025-02-17 19:35:41 -0600

    ITS WORKING! adding sample data

--- products/views.py
+++ products/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Controller
+from cart.models import Cart
 
 def home(request):
     """Homepage view with featured controllers"""
-    featured_controllers = Controller.objects.filter(is_featured=True, available=True)
+    featured_controllers = Controller.objects.filter(featured=True)
     categories = Category.objects.all()
     
     context = {
         'featured_controllers': featured_controllers,
         'categories': categories,
     }
+    
+    # Add cart to context if user is authenticated
+    if request.user.is_authenticated:
+        try:
+            cart = Cart.objects.get(user=request.user)
+            context['cart'] = cart
+        except Cart.DoesNotExist:
+            pass
+            
     return render(request, 'products/home.html', context)
 
 def category_detail(request, slug):
